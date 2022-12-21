3099d1163191c3fab93f48e51463a952a170c7f4
Junya-0220 <junya010220@gmail.com>
2022-12-21 22:45:06 +0900

    DBの接続をインターフェイスを通じて行うように修正

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"database/sql"
+	"backend/internal/models/repository"
+	"backend/internal/models/repository/dbrepo"
 	"flag"
 	"fmt"
 	"log"
@@ -13,7 +14,7 @@ const port = 8080
 type application struct {
 	DSN string
 	Domain string
-	DB *sql.DB
+	DB repository.DatabaseRepo
 }
 
 
@@ -30,7 +31,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	app.DB = conn
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
 
 	app.Domain = "example.com"
 
